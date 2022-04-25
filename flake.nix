45bb427ace112608ba7d919399de3288dab9fac9
Nazar Vinnichuk <nazar@vinnich.uk>
2022-04-25 13:43:51 +0300

    Avoid unbound function calls in flake.nix.

--- flake.nix
+++ flake.nix
@@ -3,15 +3,17 @@
 
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     with import nixpkgs { inherit system; }; {
-      devShell = pkgs.mkShell {
+      devShell = with {
+        server = pkgs.writeShellScriptBin "server" ''
+          ls *.md | entr pweave -f md2html /_ &
+          python -m http.server
+        '';
+      }; pkgs.mkShell {
         packages = [
           pkgs.python310
           pkgs.python310Packages.Pweave
           pkgs.entr
-          (pkgs.writeShellScriptBin "server" ''
-            ls *.md | entr pweave -f md2html /_ &
-            python -m http.server
-          '')
+          server
         ];
       };
     }
