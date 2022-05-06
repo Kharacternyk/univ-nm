fc115f1a62d40a0cf4134af0c14b2fbd998d5a0d
Nazar Vinnichuk <nazar@vinnich.uk>
2022-05-06 12:05:54 +0300

    Simplify flake.nix.

--- flake.nix
+++ flake.nix
@@ -4,15 +4,15 @@
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     with import nixpkgs { inherit system; }; {
       devShell = with {
-        server = pkgs.writeShellScriptBin "server" ''
+        server = writeShellScriptBin "server" ''
           ls *.md | entr pweave -f md2html /_ &
           python -m http.server
         '';
-      }; pkgs.mkShell {
+      }; mkShell {
         packages = [
-          pkgs.python310
-          pkgs.python310Packages.Pweave
-          pkgs.entr
+          python310
+          python310Packages.Pweave
+          entr
           server
         ];
       };
